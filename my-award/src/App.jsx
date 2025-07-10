22605c5caa0db54590d4dd72ce3947c23f64ca18
saikat <mondalsaikatkumar@gmail.com>
2025-07-10 17:24:38 +0600

    implement the first part of hero section

--- my-award/src/App.jsx
+++ my-award/src/App.jsx
@@ -2,14 +2,19 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import "./App.css";
 import gsap from "gsap";
+import Navbar from "./componets/Navbar";
+import Hero from "./componets/Hero";
 
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return (
-    <>
-      <h1 className="text-3xl text-amber-700 flex-center">Om Namah Shivaya</h1>
-    </>
+    <main>
+      {/* <h1 className="text-3xl text-amber-700 flex-center">Om Namah Shivaya</h1> */}
+      <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" />
+    </main>
   );
 }
 
